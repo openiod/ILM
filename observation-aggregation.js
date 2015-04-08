cfd2218262801921cd3b974e9a13e20cc81737a1
awiel <awiel@scapeler.com>
2015-04-09 00:22:06 +0200

    update

--- observation-aggregation.js
+++ observation-aggregation.js
@@ -43,10 +43,8 @@ module.exports = {
 			
 			console.log('Collection: ' + collection);
 			console.log('   aggregation: ' + param.aggregation);
-			
-			var aggregation = JSON.parse(param.aggregation);
 
-			collection.aggregate(aggregation).toArray(function(err, results) {
+			collection.aggregate(param.aggregation).toArray(function(err, results) {
 				
 				if (err) {
 					console.log('mongodb find err: ' + err);

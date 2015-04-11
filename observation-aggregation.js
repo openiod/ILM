5367b350675321dbe028d70c303cc3aea565112f
awiel <awiel@scapeler.com>
2015-04-11 23:40:39 +0200

    mongo merge

--- observation-aggregation.js
+++ observation-aggregation.js
@@ -61,7 +61,56 @@ module.exports = {
 
       		});
 		});
-	}
+	},
+	
+	// Merge db.collection1.find().forEach(function(doc){db.collection2.save(doc)});
+	merge: function (featureOfInterest, param, callback) {
+		
+//		if (param.query.file != null ) {
+//			var observationFile = fs.readFileSync(airboxCsvFileName);
+//			console.log('Observation from file: ' + observationFile.length);
+//			this.createCql(observationFile, callback);
+//		} else {
+//			this.streamCsvHistoryFile (csvHistoryUrl + airboxCsvFileName, airboxCsvFileName,	false, 'aireascsvdata', callback);
+//		}
+
+		console.log('All retrieve actions are activated. getMongoData observation-aggregation: ' + featureOfInterest );
+		console.log(' Aggregation: ' + param.query );
+		MongoClient.connect('mongodb://192.168.0.92:27017/openiod', function(err, db) {
+	  	 	if(err) throw err;
+
+			var collection = db.collection(param.collection );
+			
+			console.log('Collection: ' + param.collection);
+			console.log('   aggregation: ' + JSON.stringify(param.aggregation) );
+
+			collection.aggregate(param.aggregation).toArray(function(err, results) {
+				
+				if (err) {
+					console.log('mongodb find err: ' + err);
+				}
+				
+				var collectionTmp 	= db.collection(param.collectionTmp );
+				var collectionMerge = db.collection(param.collectionMerge );
+				
+				collectionTmp.find().forEach(function(doc){collectionMerge.save(doc)});
+				
+				collectionTmp.drop();
+				
+				//var results = {};
+				//results.
+					
+				console.log('Closing the database.');
+				console.log(' Data: ' + results.length);
+				db.close();
+				callback(results);
+
+      		});
+		});
+	}	
+	
+	
+	
 
 
 };

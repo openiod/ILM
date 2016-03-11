77e46da60ff799b0d8d6859a76eb54ad62b4f6aa
awiel <awiel@scapeler.com>
2016-03-11 23:01:59 +0100

    update stream

--- airbox.js
+++ airbox.js
@@ -71,14 +71,14 @@ var teller=0;
 //			console.log(callback);
 //			setTimeout(mapCallBack(null, data), 30000);
 
-			var mapCallBackTest2 = function (data2, mapCallBack2) {
+			var mapCallBackTest2 = function (data2) {
 				console.log('test2');
 				mapCallBackTest(data2);
-				mapCallBack2(null, data2);
+			//	mapCallBack2(null, data2);
 			}
 	//		console.log(setTimeout);
-			setTimeout(mapCallBackTest2, 10000, data, mapCallBack);
-			//mapCallBack(null, data);
+			setTimeout(mapCallBackTest2, 10000, data);
+			mapCallBack(null, data);
 			//var myFunction = function callback(null, data);
 		}));
 		
